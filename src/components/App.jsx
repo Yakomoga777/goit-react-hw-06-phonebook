f4994d03cf474e938fbc104a6cf5138a57b03208
Roman Petrenko <rpetrenko0105@gmail.com>
2023-05-28 18:41:22 +0300

    Видаляє зайві імпорти в Арр

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { nanoid } from 'nanoid';
 
 import { ContactList } from './ContactList/ContactList';
 import { GlobalStyle } from './GlobalStyle/GlobalStyle';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
-import { addContact } from 'Redux/store';
-import { useDispatch } from 'react-redux';
 
 const theme = {};
 
@@ -19,61 +16,14 @@ export const INITIAL_CONTACTS = [
 ];
 
 export const App = () => {
-  // const dispatch = useDispatch();
-
-  // const [contacts, setContacts] = useState(
-  //   () =>
-  //     JSON.parse(window.localStorage.getItem('contacts')) ?? INITIAL_CONTACTS
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // // Керований інпут фільтра
-  // const handleChangeFilter = event => {
-  //   if (event.target.name === 'filter') {
-  //     setFilter(event.target.value);
-  //   }
-  // };
-
-  // // Функція фільтрації контактів
-  // const filtersContacts = () => {
-  //   if (!filter) {
-  //     return contacts;
-  //   } else if (filter) {
-  //     return contacts.filter(item =>
-  //       item.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-  //     );
-  //   }
-  // };
-
-  // //Метод видалення
-  // const handleDelete = id => {
-  //   setContacts(contacts.filter(contact => contact.id !== id));
-  // };
-
-  // //запис в localStorage
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-
       <h1>Phonebook</h1>
-      <ContactForm
-        btn="Add contact"
-        // handleSubmit={handleSubmit}
-      />
-
+      <ContactForm btn="Add contact" />
       <h2>Contacts</h2>
-      <Filter
-      // value={filter}
-      // onFilterInput={handleChangeFilter}
-      />
-      <ContactList
-      // items={filtersContacts()}
-      // handleDelete={handleDelete}
-      />
+      <Filter />
+      <ContactList />
     </ThemeProvider>
   );
 };
